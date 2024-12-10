6ae20e11754164430b165b2e3352b425842dc1b2
zts24 <zach.t.smith97@gmail.com>
2024-12-10 15:18:50 -0600

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
 import os
 import base64
 
-ins1 = 'd2hvYW1p' #whoami
-ins2 = 'cHMK' #ps
-ins3 = 'bnNsb29rdXAgYmFkZG9tYWluLmNvbQ==' #nslookup bad domain
+ins1 = 'd2hvYW1p'
+ins2 = 'cHMK'
+ins3 = 'bnNsb29rdXAgYmFkZG9tYWluLmNvbQ=='
 
 os.system(str(base64.b64decode(ins1).decode('utf-8')))
 os.system(str(base64.b64decode(ins2).decode('utf-8')))
